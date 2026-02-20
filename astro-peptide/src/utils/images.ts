29bd7f41e952a30b205cf2e812b1fa92be1ffd18
Manuel Dax <mister.manuel94@gmail.com>
2026-02-20 16:09:27 +0000

    Fix image display: stop .png to .webp conversion, update hero refs to .png

--- astro-peptide/src/utils/images.ts
+++ astro-peptide/src/utils/images.ts
@@ -1,5 +1,4 @@
 export const toOptimizedImage = (src: string): string => {
   if (!src) return src;
-  if (/^https?:\/\//i.test(src)) return src;
-  return src.replace(/\.png$/i, '.webp');
+  return src;
 };
